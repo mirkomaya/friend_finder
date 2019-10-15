e04a3c9181c76326339060e453d3525ebecb9320
Marty McFly <mirko.maya@live.com>
2019-10-14 20:00:28 -0400

    Testing routes.

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var express = require("express");
 
-var path = require("path");
+var app = express();
 
+// Port
+var PORT = process.env.PORT || 4440;
+
+// Data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
